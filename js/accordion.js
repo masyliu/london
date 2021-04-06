47539cb035c970ad74e56cde031107e5e1d296d7
masy <taurusmasy@yahoo.com.hk>
2021-04-07 04:03:34 +0800

    accordion js, color, font

---
+++ js/accordion.js
@@ -0,0 +1,21 @@
+const accordions = document.querySelectorAll('.accordion');
+let opened = document.querySelector('.open');
+handlePanel(opened);
+
+function handlePanel(n) {
+  let currentPanel = n.nextElementSibling;
+  currentPanel.style.display = 'block';
+}
+
+function handleAccordion() {
+  const panels = document.querySelectorAll('.panel');
+  for (let i = 0; i < panels.length; i++) {
+    panels[i].style.display = 'none';
+  }
+  opened.classList.remove('open');
+  this.classList.add('open');
+  opened = this;
+  handlePanel(opened);
+}
+
+accordions.forEach(accordion => accordion.addEventListener('click', handleAccordion));
